d198b489411daa61072691b834aefd0c716f84dd
ohavbenaroya-crypto <ohavbenaroya@gmail.com>
2026-09-08 15:11:18 +0300

    solider

--- soldier.py
+++ soldier.py
@@ -3,5 +3,7 @@ import pygame
 
 def create_solider():
     return {"img": pygame.image.load('soldier.png'),
-            "start_x": 0,
-            "start_y": 0,}
+            "list_of_body_position":[(0,0),(1,0),(0,1),(1,1),(0,2),(1,2)] ,
+            "list_of_legs_position":[(0,3),(1,3)],
+
+    }
