f894ae4addc768117ad537734e171f651b6e1cff
ohavbenaroya-crypto <ohavbenaroya@gmail.com>
2026-09-08 12:08:42 +0300

    screen

--- screen.py
+++ screen.py
@@ -1,8 +1,10 @@
+import random
+
 import pygame
 
-WINDOW_WIDTH=50*20
-WINDOW_HEIGHT=25*20
-BACKGROUND_COLOR = (138, 201, 38)
+from consts import WINDOW_WIDTH, WINDOW_HEIGHT
+
+BACKGROUND_COLOR = (0,100,0)
 
 screen = pygame.display.set_mode(
         (WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -10,10 +12,36 @@ def open_screen():
         background_color = (BACKGROUND_COLOR)
         pygame.display.set_caption('shanni and talya')
         screen.fill(background_color)
+        print_random_grass()
         pygame.display.flip()
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+
+''' PRINTS 20 GRASSESS  '''
+def print_random_grass():
+    img=pygame.image.load('grass.png')
+    num_of_grass=20
+    img = pygame.transform.scale(img, (60, 40))
+    running = True
+    list_of_grass_locations = []
+    for i in range(num_of_grass):
+        x = random.randrange(1, WINDOW_WIDTH,60)
+        while x+60>WINDOW_WIDTH:
+            x = random.randrange(1, WINDOW_WIDTH, 60)
+        y = random.randrange(1, WINDOW_HEIGHT, 40)
+        while y + 40 > WINDOW_HEIGHT:
+              y = random.randrange(1, WINDOW_HEIGHT,40)
+        list_of_grass_locations.append((x,y))
+    while running:
+        for pos in list_of_grass_locations:
+            screen.blit(img,pos)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
 open_screen()
