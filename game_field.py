6d3cafa23c9b51e74485309c5e2d818615bbb9f3
daniel <danielkagan00@gmail.com>
2026-09-09 12:08:59 +0300

    מוקשים במטריצה

--- game_field.py
+++ game_field.py
@@ -1,5 +1,11 @@
 # region--------------------IMPORTS-----------------------
+from turtledemo.clock import jump
+
 import consts
+import random
+
+from consts import BOARD_ROWS, BOARD_COLS, MINE_ROWS, MINE_COLS, MINES_COUNT
+
 
 # endregion-----------------------------------------------
 
@@ -9,19 +15,54 @@ def create():
     game_field = [["FREE" for col in range(consts.BOARD_COLS)] for row in range(consts.BOARD_ROWS)]
     add_soldier_to_matrix(game_field)
     add_flag_to_matrix(game_field)
+    add_random_mines(game_field)
     return game_field
 
+''' add soldier to the left top in the matrix'''
 def add_soldier_to_matrix(game_field):
+    # add soldier body
     for i in range(consts.SOLDIER_BODY_ROWS):
         for j in range(consts.SOLDIER_COLS):
             game_field[i][j] = "SOLDIER_BODY"
-
+    # add soldier feet
     for i in range(consts.SOLDIER_BODY_ROWS, consts.SOLDIER_BODY_ROWS + consts.SOLDIER_FEET_ROWS):
         for j in range(consts.SOLDIER_COLS):
             game_field[i][j] = "SOLDIER_FEET"
 
+''' add fleg to the right bottom in the matrix'''
 def add_flag_to_matrix(game_field):
     for i in range(consts.BOARD_ROWS - consts.FLAG_ROWS, consts.BOARD_ROWS):
         for j in range(consts.BOARD_COLS - consts.FLAG_COLS, consts.BOARD_COLS):
             game_field[i][j] = "FLAG"
+
+''' add mines in random locations to the matrix'''
+def add_random_mines(game_field):
+    for i in range(consts.MINES_COUNT):
+
+        mine_row = get_random_matrix_location(1,BOARD_ROWS , MINE_ROWS)
+        mine_col = get_random_matrix_location(1, BOARD_COLS,MINE_COLS)
+        for i in range(MINES_COUNT):
+            while not check_free(game_field, mine_row, mine_col):
+                check_free(game_field, mine_row, mine_col)
+            add_mines_to_loc(game_field, mine_row, mine_col)
+
+
+def add_mines_to_loc(game_field ,row , col):
+    for i in range(row, row + MINE_ROWS):
+        for j in range(col,col + MINE_COLS):
+            game_field[i][j] == "MINE"
+
+def check_free(game_field, row, col):
+    is_empty = True
+    for i in range(row, row + MINE_ROWS):
+        for j in range(col, col + MINE_COLS):
+            if game_field[i][j] != "FREE":
+                return False
+    return True
+
+def get_random_matrix_location(start, range, jumps):
+    random_in_range = random.randrange(start, range, jumps)
+    while random_in_range + jumps > range:
+        random_in_range = random.randrange(start, BOARD_ROWS, MINE_ROWS)
+    return random_in_range
 
